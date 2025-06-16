2fab35f0bc6219a1ecfe1f0ebd2bbe63435117ea
Chronoss0518 <sinyusuke5000@gmail.com>
2025-06-17 00:22:39 +0900

    重ねて表示するWidgetの画面の表示タイプを変更するためのクラスを仮作成(中身は無し

---
+++ lib/component/component_parts/select_board_type.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:gaming_calculator/component/base_component/on_window_size_base.dart';
+
+class SelectBoardType extends StatelessWidget
+{
+  SelectBoardType({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return OnWindowSizeBase(Container(color: Colors.blue));
+  }
+}
