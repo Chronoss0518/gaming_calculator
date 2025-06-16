62a29307919cb5c0794276bb882b9471b3d75204
Chronoss0518 <sinyusuke5000@gmail.com>
2025-06-17 00:23:00 +0900

    2人用のデータパネルを表示するための画面の作成

---
+++ lib/component/scene/member_board_2.dart
@@ -0,0 +1,14 @@
+import 'package:ch_flutter_library/widget/scene_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:gaming_calculator/component/component_parts/select_board_type.dart';
+import 'package:gaming_calculator/component/scene/open_on_window_scene.dart';
+
+class MemberBoard2 extends OpenOnWindowScene
+{
+  @override
+  Widget buildBase(BuildContext context) {
+    
+    setOnWidget(SelectBoardType());
+    return Container(color: Colors.red,);
+  }
+}
