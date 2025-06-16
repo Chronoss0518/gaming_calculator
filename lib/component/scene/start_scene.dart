8c63ee05f9175ac0b7c68e81e630317ad0caec5e
Chronoss0518 <sinyusuke5000@gmail.com>
2025-06-16 14:48:20 +0900

    BaseSceneを継承したStartSceneの作成準備

---
+++ lib/component/scene/start_scene.dart
@@ -0,0 +1,13 @@
+
+import 'package:ch_flutter_library/widget/scene_manager.dart';
+import 'package:flutter/material.dart';
+
+
+class StartScene extends BaseScene
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Container();
+  }
+}
