f694d4a0d6e4575df6e2d7e66952b0ef30a25bdc
Chronoss0518 <sinyusuke5000@gmail.com>
2025-06-18 12:22:55 +0900

    一旦現在あるデータを全てコミット

--- lib/component/component_parts/data_panel.dart
+++ lib/component/component_parts/data_panel.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 class DataPanel extends StatelessWidget
 {
+  const DataPanel(this._dataPanelNo,{super.key});
+
+  final int _dataPanelNo;
+
   @override
   Widget build(BuildContext context) {
     return Container();
--- lib/component/scene/start_scene.dart
+++ lib/component/scene/start_scene.dart
@@ -1,13 +1,16 @@
 
 import 'package:ch_flutter_library/widget/scene_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:gaming_calculator/component/component_parts/select_board_type.dart';
+import 'package:gaming_calculator/component/scene/open_on_window_scene.dart';
 
 
-class StartScene extends BaseScene
+class StartScene extends OpenOnWindowScene
 {
   @override
-  Widget build(BuildContext context)
+  Widget buildBase(BuildContext context)
   {
+    setOnWidget(SelectBoardType());
     return Container();
   }
 }
