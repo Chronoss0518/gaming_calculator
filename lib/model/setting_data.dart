689092bf7807175452f15f115c35e91f3569f638
Chronoss0518 <sinyusuke5000@gmail.com>
2025-06-18 12:23:57 +0900

    DataPaneの挙動に対する情報をまとめたクラスを仮作成

---
+++ lib/model/setting_data.dart
@@ -0,0 +1,32 @@
+class SettingData
+{
+  get maxPoint => _maxPoint;
+  get minPoint => _minPoint;
+  get maxCount => _maxCount;
+  
+  void setMaxPoint(int point)
+  {
+    if(_minPoint >= point)return;
+    _maxPoint = point;
+  }
+
+  void setMinPoint(int point)
+  {
+    if(_maxPoint <= point)return;
+    _minPoint = point;
+  }
+
+  void setMaxCount(int count)
+  {
+    if(count < 0)return;
+    _maxCount = count;
+  }
+
+//Private Member//
+
+  int _maxPoint = 0;
+  int _minPoint = 0;
+  int _maxCount = 0;
+
+
+}
