94618853773700ad0cc77ac5eacd86db46e32ac1
Chronoss0518 <sinyusuke5000@gmail.com>
2025-06-19 23:25:08 +0900

    表示されているDataPanelの情報を反転させる機能を持つButtonを作成

--- lib/component/component_parts/button_inverse_data_panel.dart
+++ lib/component/component_parts/button_inverse_data_panel.dart
@@ -2,18 +2,37 @@ import 'package:ch_flutter_library/widget/scene_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:gaming_calculator/model/display_setting_data.dart';
 
+const double ICON_OVER_SIZE = 10;
+
 class ButtonInverseDataPanel extends StatelessWidget
 {
-  ButtonInverseDataPanel(this.displayScene,{super.key});
+  ButtonInverseDataPanel(this.displayScene,{this.baseSize,super.key});
 
   final BaseScene displayScene;
+  final double? baseSize;
 
   @override
   Widget build(BuildContext context) {
+
+    var tmpSize = baseSize;
+    if(tmpSize != null)tmpSize = tmpSize + ICON_OVER_SIZE;
     return MaterialButton(
       onPressed: (){
-        //var displaySetting = displayScene.getSaveData<DisplaySettingData>();
+        displayScene.setState((){
+          var displaySetting = displayScene.getSaveData<DisplaySettingData>();
+          displaySetting?.inversionFlg = !displaySetting.inversionFlg;
+        });
       },
-      child: Container());
+      child: Container(
+        width: tmpSize,
+        height: tmpSize,
+        decoration: BoxDecoration(border: Border.all(width: 5.0)),
+        child: Center(
+          child: Icon(
+            Icons.change_circle_outlined,size: baseSize,
+          ),
+        )
+      )
+    );
   }
 }
