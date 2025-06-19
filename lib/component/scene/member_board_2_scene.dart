1365eb817198b70381e19f95ad9fa471c2d03c71
Chronoss0518 <sinyusuke5000@gmail.com>
2025-06-19 16:17:50 +0900

    ついでに各画面の名称にはSceneを付けるように修正

--- lib/component/scene/member_board_2_scene.dart
+++ lib/component/scene/member_board_2_scene.dart
@@ -4,7 +4,7 @@ import 'package:gaming_calculator/component/component_parts/data_panel.dart';
 import 'package:gaming_calculator/component/scene/open_on_window_widget.dart';
 import 'package:gaming_calculator/model/application_model_manager.dart';
 
-class MemberBoard2 extends BaseScene
+class MemberBoard2Scene extends BaseScene
 {
 
   var appModelManager = ApplicationModelManager();
