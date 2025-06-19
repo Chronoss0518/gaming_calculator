d345b340429200065dc4a4d694f6b18b4e147e29
Chronoss0518 <sinyusuke5000@gmail.com>
2025-06-19 20:14:21 +0900

    基本的なサイズを外から変更できるように修正

--- lib/component/component_parts/data_panel.dart
+++ lib/component/component_parts/data_panel.dart
@@ -3,14 +3,52 @@ import 'package:gaming_calculator/model/application_model_manager.dart';
 
 class DataPanel extends StatelessWidget
 {
-  DataPanel(this._dataPanelNo,{super.key});
+  DataPanel(
+    this.dataPanelNo,
+    {this.baseSize = 12,
+      this.width,
+      this.height,
+      super.key});
 
-  final int _dataPanelNo;
+  final int dataPanelNo;
+  final double baseSize;
+  final double? width;
+  final double? height;
+
+
+  static const double nickNameTextSize = 2.0; 
+  static const double pointTextSize = 3.0; 
   
   final appModelManager = ApplicationModelManager();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var playerData = appModelManager.getPlayerData(dataPanelNo);
+
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      width: width,
+      height: height,
+      decoration: BoxDecoration(border: Border.all(width: 10,)),
+      child:playerData != null ? 
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        Container(
+          margin:EdgeInsets.all(1.0),
+          child: Text(
+            playerData.nickName,
+            style: TextStyle(fontSize: nickNameTextSize * baseSize),
+            textAlign: TextAlign.left)),
+        Container(
+          margin:EdgeInsets.all(1.0),
+          child: Text(
+            playerData.point.toString(),
+            style: TextStyle(fontSize: pointTextSize * baseSize,),
+            textAlign: TextAlign.left,),
+        ),
+
+      ],) : null,
+    );
   }
 }
