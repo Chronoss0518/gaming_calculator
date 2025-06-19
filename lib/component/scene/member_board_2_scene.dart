49ad330718ba5d092ee66a6314bb8470bc728864
Chronoss0518 <sinyusuke5000@gmail.com>
2025-06-19 23:25:51 +0900

    1画面に2つのDataPanelを表示する画面の土台を作成

--- lib/component/scene/member_board_2_scene.dart
+++ lib/component/scene/member_board_2_scene.dart
@@ -1,23 +1,45 @@
+import 'dart:ui';
+
 import 'package:ch_flutter_library/widget/scene_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:gaming_calculator/component/component_parts/button_inverse_data_panel.dart';
 import 'package:gaming_calculator/component/component_parts/data_panel.dart';
+import 'package:gaming_calculator/component/scene/member_board_scene_base.dart';
 import 'package:gaming_calculator/component/sub_window/open_on_window_widget.dart';
-import 'package:gaming_calculator/component/sub_window/select_board_type.dart';
 import 'package:gaming_calculator/model/application_model_manager.dart';
+import 'package:gaming_calculator/model/display_setting_data.dart';
 
-class MemberBoard2Scene extends BaseScene
+class MemberBoard2Scene extends MemberBoardSceneBase
 {
 
   var appModelManager = ApplicationModelManager();
 
   @override
+  void init({SaveData? sendData}) {
+    super.init(sendData: sendData);
+    appModelManager.addDataPanelData("Test1");
+    appModelManager.addDataPanelData("Test2");
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var displaySetting = getSaveData<DisplaySettingData>();
     var baseWidget = OpenOnWindowWidget(
-      DataPanel(0,
-      inversionFlg: false,
-      baseSize: 12,
-      width: 200,
-      height: 200,));
+      Row(
+        children: [
+          DataPanel(0,
+          inversionFlg: displaySetting?.inversionFlg ?? false,
+          baseSize: 12,
+          width: 200,
+          height: 200,),
+          ButtonInverseDataPanel(this,baseSize: 64.0,),
+          DataPanel(1,
+          inversionFlg: displaySetting?.inversionFlg ?? false,
+          baseSize: 12,
+          width: 200,
+          height: 200,),
+        ],
+      ));
     return baseWidget;
   }
 }
