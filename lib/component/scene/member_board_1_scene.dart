f049b1d65258efe43c900b1318c96e3a4beca42d
Chronoss0518 <sinyusuke5000@gmail.com>
2025-06-20 08:56:28 +0900

    1データパネル編集Widgetと各ボタンが配置されている画面の作成

---
+++ lib/component/scene/member_board_1_scene.dart
@@ -0,0 +1,39 @@
+import 'dart:ui';
+
+import 'package:ch_flutter_library/widget/scene_manager.dart';
+import 'package:flutter/material.dart';
+import 'package:gaming_calculator/component/component_parts/button_inverse_data_panel.dart';
+import 'package:gaming_calculator/component/component_parts/data_panel_editor.dart';
+import 'package:gaming_calculator/component/scene/member_board_scene_base.dart';
+import 'package:gaming_calculator/component/sub_window/open_on_window_widget.dart';
+import 'package:gaming_calculator/model/application_model_manager.dart';
+import 'package:gaming_calculator/model/display_setting_data.dart';
+
+class MemberBoard1Scene extends MemberBoardSceneBase
+{
+
+  var appModelManager = ApplicationModelManager();
+
+  @override
+  void init({SaveData? sendData}) {
+    super.init(sendData: sendData);
+    appModelManager.initDataPanelData(0);
+    appModelManager.addDataPanelData("Test1");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var displaySetting = getSaveData<DisplaySettingData>();
+    var baseWidget = OpenOnWindowWidget(
+      Row(
+        children: [
+          ButtonInverseDataPanel(this,baseSize: 64.0,),
+          DataPanelEditor(0,
+          inversionFlg: displaySetting?.inversionFlg ?? false,
+          displayCloseButtonFlg: false,),
+          ButtonInverseDataPanel(this,baseSize: 64.0,),
+        ],
+      ));
+    return baseWidget;
+  }
+}
