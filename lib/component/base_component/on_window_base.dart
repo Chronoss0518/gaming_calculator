eba6a9c1b8845fc5e1a4f0a6b9c473c1e31bf03c
Chronoss0518 <sinyusuke5000@gmail.com>
2025-06-19 21:01:24 +0900

    OnWIndowSizeBaseからOnWindowBaseへ変更し、close機能を追加するように修正

---
+++ lib/component/base_component/on_window_base.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+import 'package:gaming_calculator/component/component_parts/open_on_window_widget.dart';
+
+class OnWindowBase extends StatelessWidget
+{
+  const OnWindowBase(this.displayWidget,{this.child,super.key});
+
+  final Widget? child;
+  final OpenOnWindowWidget displayWidget;
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      heightFactor: 0.8,
+      widthFactor: 0.8,
+      child: Stack(
+        children: [
+          child?? Container(),
+          MaterialButton(
+            child:Icon(Icons.close),
+            onPressed: displayWidget.closeOnWidget),
+        ])
+      );
+  }
+
+}
