dac27ed64432cffafb20d42b10be825c48b463e3
Chronoss0518 <sinyusuke5000@gmail.com>
2025-06-19 16:19:26 +0900

    こちらも変更内容を適用

--- lib/component/scene/member_board_2_scene.dart
+++ lib/component/scene/member_board_2_scene.dart
@@ -1,7 +1,7 @@
 import 'package:ch_flutter_library/widget/scene_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:gaming_calculator/component/component_parts/data_panel.dart';
-import 'package:gaming_calculator/component/scene/open_on_window_widget.dart';
+import 'package:gaming_calculator/component/component_parts/open_on_window_widget.dart';
 import 'package:gaming_calculator/model/application_model_manager.dart';
 
 class MemberBoard2Scene extends BaseScene
--- lib/component/scene/start_scene.dart
+++ lib/component/scene/start_scene.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:gaming_calculator/component/base_component/on_window_size_base.dart';
 import 'package:gaming_calculator/component/component_parts/data_panel.dart';
 import 'package:gaming_calculator/component/component_parts/select_board_type.dart';
-import 'package:gaming_calculator/component/scene/open_on_window_widget.dart';
+import 'package:gaming_calculator/component/component_parts/open_on_window_widget.dart';
 
 
 class StartScene extends BaseScene
