850b24134c2bcea94c5ba7625f65e84b5ed93723
Chronoss0518 <sinyusuke5000@gmail.com>
2025-06-20 16:44:46 +0900

    DataPanelの値を変更するための数字パネルコンポネントを準備

---
+++ lib/component/base_component/number_panel.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NumberPanel extends StatelessWidget
+{
+  NumberPanel(this.number,{super.key});
+
+  final int number;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
