4a232083b19e04f147d7268e3e573e05ae0f7833
Chronoss0518 <sinyusuke5000@gmail.com>
2025-07-01 01:02:26 +0900

    管理するクラスに合わせるクラスを作成

--- lib/component/component_parts/button_inverse_data_panel.dart
+++ lib/component/component_parts/button_inverse_data_panel.dart
@@ -1,39 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:gaming_calculator/component/scene/member_board_scene_base.dart';
+import 'package:gaming_calculator/component/component_parts/data_panel_editor_buttons.dart';
 import 'package:gaming_calculator/model/display_setting_data.dart';
 
-const double ICON_OVER_SIZE = 10;
-
-class ButtonInverseDataPanel extends StatelessWidget
+class ButtonInverseDataPanel extends DataPanelEditorButtonBase
 {
-  ButtonInverseDataPanel(this.displayScene,{this.baseSize,super.key});
-
-  final MemberBoardSceneBase displayScene;
-  final double? baseSize;
-
-  @override
-  Widget build(BuildContext context) {
-
-    var tmpSize = baseSize;
-    if(tmpSize != null)tmpSize = tmpSize - ICON_OVER_SIZE;
-    return MaterialButton(
-      onPressed: (){
-        displayScene.setState((){
+  ButtonInverseDataPanel(
+    super.displayScene,
+    super.baseSize,{
+      super.key})
+  {
+    super.act = (){
           var displaySetting = displayScene.getSaveData<DisplaySettingData>();
           displaySetting?.inversionFlg = !displaySetting.inversionFlg;
-        });
-      },
-      child: Container(
-        width: baseSize,
-        height: baseSize,
-        margin: EdgeInsets.only(top:5.0,bottom:5.0),
-        decoration: BoxDecoration(border: Border.all(width: 5.0)),
-        child: Center(
-          child: Icon(
-            Icons.change_circle_outlined,size: tmpSize,
-          ),
-        )
-      )
-    );
+        };
+    
+    final tmpSize = baseSize * ICON_SIZE_RATIO;
+    super.childImage = Icon(Icons.change_circle_outlined,size: tmpSize,);
   }
 }
