81fd9984d759c1386a15e5fc160073a57aaac404
Chronoss0518 <sinyusuke5000@gmail.com>
2025-07-01 01:27:15 +0900

    PointEditorの表示を修正

--- lib/component/component_parts/point_editor.dart
+++ lib/component/component_parts/point_editor.dart
@@ -16,27 +16,37 @@ class _State extends State<PointEditor>
 {
 
   int value = 0;
+  TextEditingController controller = TextEditingController(text:"0");
 
   @override
   Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+    var tmpSize = (size.width < size.height ? size.width : size.height)   * 0.05;
+
     return Column(
       children: [
         Expanded(
-          child: Text(value.toString()),
+          child: TextField(controller:controller,onChanged: (value) {
+            this.value = int.tryParse(value) ?? 0;
+          },
+          keyboardType: TextInputType.numberWithOptions(decimal: true),
+          style:TextStyle(fontSize: tmpSize)),
         ),
         Expanded(
-          child: Text(value.toString()),
-        ),
-        Expanded(
-          flex: 3,
-          child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-            itemBuilder: (context,count){
-              return NumberPanel((count + 1) % 10, 10, (){
-                widget.displayScene.setState((){value *= 10;value += (count + 1) % 10;});
-              });
-            },
-            itemCount: 10,
+          flex: 5,
+          child: AspectRatio(
+            aspectRatio: 0.7,
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+              
+              itemBuilder: (context,count){
+                return NumberPanel((count + 1) % 10, tmpSize, (){
+                  widget.displayScene.setState((){value *= 10;value += (count + 1) % 10;});
+                });
+              },
+              itemCount: 10,
+            ),
           ),
         )
       ],
