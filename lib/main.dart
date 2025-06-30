5678ac24093747b5453df7f27afdd022515779c4
Chronoss0518 <sinyusuke5000@gmail.com>
2025-07-01 01:06:58 +0900

    _sceneManagerを関数外で初期化する修正

--- lib/main.dart
+++ lib/main.dart
@@ -3,29 +3,34 @@ import 'package:ch_flutter_library/widget/scene_manager.dart';
 import 'package:gaming_calculator/component/scene/member_board_1_scene.dart';
 import 'package:gaming_calculator/component/scene/member_board_2_scene.dart';
 import 'package:gaming_calculator/component/scene/start_scene.dart';
+import 'package:gaming_calculator/model/application_constant.dart';
 import 'package:gaming_calculator/model/application_model_manager.dart';
 
-late SceneManager _sceneManager; 
+SceneManager _sceneManager
+  = SceneManager(MemberBoard1Scene());
+//  = SceneManager(MemberBoard2Scene());
 
 void main() {
-  _sceneManager = SceneManager(MemberBoard1Scene());
-  //_sceneManager = SceneManager(MemberBoard2Scene());
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       //title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: _sceneManager,
+      home: SafeArea(
+        child: Center(
+          child: _sceneManager,
+        )
+      ),
     );
   }
 }
