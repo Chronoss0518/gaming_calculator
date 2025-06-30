e89e31e45b1ed1f3c87cdd1614386efc2f8de2c6
Chronoss0518 <sinyusuke5000@gmail.com>
2025-07-01 01:04:11 +0900

    画面上に表示されるPointを操作するための計算機を表示する機能を修正

--- lib/component/base_component/number_panel.dart
+++ lib/component/base_component/number_panel.dart
@@ -2,12 +2,24 @@ import 'package:flutter/material.dart';
 
 class NumberPanel extends StatelessWidget
 {
-  NumberPanel(this.number,{super.key});
+  NumberPanel(
+    this.number,
+    this.size,
+    this.act,{
+      super.key});
 
   final int number;
+  final double size;
+  final void Function() act;
   
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final tmpSize = size * 0.8;
+    return Container(
+      width : size,
+      height : size,
+      decoration: BoxDecoration(border: Border.all(width:5)),
+      child:Text(number.toString(),style:TextStyle(fontSize: tmpSize),textAlign: TextAlign.center,),
+    );
   }
 }
--- lib/component/component_parts/point_editor.dart
+++ lib/component/component_parts/point_editor.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:gaming_calculator/component/base_component/number_panel.dart';
+import 'package:gaming_calculator/component/scene/member_board_scene_base.dart';
 
-class PointEditor extends StatelessWidget
+class PointEditor extends StatefulWidget
 {
 
-    PointEditor({this.ratio = 0,super.key});
+  PointEditor(this.displayScene,{super.key});
     
-    final double ratio;
-    @override
-    Widget build(BuildContext context) {
-        return Container(
-            width: 10,
-            height: 10,
-        );
-    }
+  final MemberBoardSceneBase displayScene;
+  @override
+  State<StatefulWidget> createState() => _State();
+}
+
+class _State extends State<PointEditor>
+{
+
+  int value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: Text(value.toString()),
+        ),
+        Expanded(
+          child: Text(value.toString()),
+        ),
+        Expanded(
+          flex: 3,
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+            itemBuilder: (context,count){
+              return NumberPanel((count + 1) % 10, 10, (){
+                widget.displayScene.setState((){value *= 10;value += (count + 1) % 10;});
+              });
+            },
+            itemCount: 10,
+          ),
+        )
+      ],
+    );
+  }
 }
