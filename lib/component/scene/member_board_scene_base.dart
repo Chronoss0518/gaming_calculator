90fb2ba557cec8195a64fa20c826cb2da8dba436
Chronoss0518 <sinyusuke5000@gmail.com>
2025-07-02 18:05:23 +0900

    動的変更し、算出する機能を追加

--- lib/component/scene/member_board_scene_base.dart
+++ lib/component/scene/member_board_scene_base.dart
@@ -1,4 +1,6 @@
 import 'package:ch_flutter_library/widget/scene_manager.dart';
+import 'package:gaming_calculator/model/application_model_manager.dart';
+import 'package:gaming_calculator/model/data_panel_data.dart';
 import 'package:gaming_calculator/model/display_setting_data.dart';
 
 abstract class MemberBoardSceneBase extends BaseScene
@@ -9,6 +11,17 @@ abstract class MemberBoardSceneBase extends BaseScene
     var tmp = DisplaySettingData();
     tmp.inversionFlg = false;
     setSaveData(tmp);
+
+    update = (){
+      final panelCount = appModelManager.getDataPanelDataCount;
+      repaint(() {
+        for(int count = 0; count <panelCount ;count++){
+          appModelManager.getDataPanelData(count)?.update();
+        }
+      },);
+    };
+    
+    setFps(120);
   }
 
 }
